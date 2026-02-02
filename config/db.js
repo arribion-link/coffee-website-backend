3156e1451934e749e545b08db68c22232f032bde
Dev Jeff <arribion@gmail.com>
2026-02-02 04:54:25 +0300

    change from local compass databse to cloud atlas

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 import "dotenv/config";
-const MONGO_URI = process.env.MONGO_URI;
-if (!MONGO_URI) {
-    console.log("error fetching database connection string");
-    process.exit(1);
+// const MONGO_URI = process.env.MONGO_URI;
+const MONGO_ATLAS_URI = process.env.MONGO_ATLAS_URI;
+if (!MONGO_ATLAS_URI) {
+  console.log("error fetching database connection string");
+  process.exit(1);
 }
 const connect_db = async () => {
     try {
-        const conn = await mongoose.connect(MONGO_URI);
+        const conn = await mongoose.connect(MONGO_ATLAS_URI);
         console.log(conn.connection.host, 'database connected successfully...');
     } catch (error) {
-        console.log(error);
+        console.log("ERROR :" , error);
     }
 }
 
